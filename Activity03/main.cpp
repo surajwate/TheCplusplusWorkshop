7f39bbd25bc493683ff66855a3120f229bad084f
Suraj Wate <suraj.wate@gmail.com>
2021-07-15 15:09:53 +0530

    activity 3

--- Activity03/main.cpp
+++ Activity03/main.cpp
@@ -7,13 +7,13 @@
 class Person
 {
 public:
-    std::string name;
-    int age;
     std::vector<std::string> userNames{};
     std::vector<int> userAge{};
 
     void addRecord()
     {
+        std::string name = "";
+        int age = 0;
         std::cout << "Add User. Please enter user name and age:" << std::endl
                   << "Name: ";
         std::cin >> name;
@@ -28,26 +28,28 @@ public:
     void fetchRecord()
     {
         int userID;
-        std::cout << "Please enter user ID:" << std::endl
+        std::cout << "\nPlease enter user ID:" << std::endl
                   << "User ID: ";
         std::cin >> userID;
-        std::cout << "User Name: " << userNames[userID];
-        std::cout << "User Age: " << userAge[userID];
+        std::cout << "User Name: " << userNames[userID] << std::endl;
+        std::cout << "User Age: " << userAge[userID] << std::endl;
     }
 };
 
 int main()
 {
-    std::cout << "User SignUp Application" << std::endl << std::endl;
+    std::cout << "User SignUp Application" << std::endl
+              << std::endl;
     bool run = true;
     int option;
     Person office;
     while (run)
     {
-        std::cout << "Please select an option:" << std::endl
+        std::cout << "\nPlease select an option:" << std::endl
                   << "1: Add Record" << std::endl
                   << "2: Fetch Record" << std::endl
-                  << "3: Quit" << std::endl << std::endl
+                  << "3: Quit" << std::endl
+                  << std::endl
                   << "Enter option: ";
 
         std::cin >> option;
